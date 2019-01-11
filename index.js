9f61598dc6fd08a2d7e1242f43155c2dba92562e
cwhsu <cwhsu1984@gmail.com>
2019-01-11 14:34:51 +0800

    Add add entry

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+let request = require('request');
+let apiKey = process.env.APIKEY;
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+request(url, function (err, response, body) {
+    if(err){
+        console.log('error:', error);
+    } else {
+        //console.log('body:', body);
+        let weather = JSON.parse(body);
+
+        let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        console.log(message);
+    }
+});
